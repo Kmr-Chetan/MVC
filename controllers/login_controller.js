fe6fadafdb45b36962bfafea3a5666a3f591c1d2
chetan kumar <ckumarhcu@gmail.com>
2023-07-04 23:28:38 +0530

    profile route

--- controllers/login_controller.js
+++ controllers/login_controller.js
@@ -11,35 +11,86 @@ module.exports.signup = (req, res) => {
     title: "Sign Up",
   });
 };
+
 module.exports.create = async (req, res) => {
-    console.log("aaa", req.body);
-  const { username,  email, password, Confirm_Password } = req.body;
+  console.log("aaa", req.body);
+  const { username, email, password, Confirm_Password } = req.body;
   if (password !== Confirm_Password) {
     return res.redirect("back");
   }
   User.findOne({ email: email })
-  .then((user) => {
-    if (user) {
-      return res.status(400).send(req.body.email + 'already exists');
-    } else {
-      User
-        .create({
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.email + "already exists");
+      } else {
+        User.create({
           name: username,
           password: password,
           email: email,
         })
-        .then((user) =>{
-            return res.status(201).redirect('/login');
-         })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
-      })
-    }
-  })
-  .catch((error) => {
-    console.error(error);
-    res.status(500).send('Error: ' + error);
-  });
-
+          .then((user) => {
+            return res.status(201).redirect("/login");
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error: " + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error);
+    });
 };
+
+module.exports.createSession = (req, res) => {
+  const { username, password } = req.body;
+  
+  User.findOne({ email: username }).then((user) => {
+    if (user) {
+      console.log("re",  user._id);
+      if (user.password !== password) {
+        return res.redirect("back");
+      }
+       res.cookie("user_id", user.id );
+       return res.redirect('/profile');
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
+
+module.exports.profile = (req, res) => {
+  console.log("cookies", req.cookies);
+  const { user_id }= req.cookies;
+  if(!user_id){
+    return res.redirect('/login');
+  }
+  User.findOne({ _id: user_id}).then((user) => {
+    if (user) {
+       res.cookie("user_id", user.id );
+       return res.render('user_profile',{
+        title:"user profile",
+        user: user.name,
+        email: user.email
+       });
+    } else {
+      return res.redirect("/login");
+    }
+  }).catch((err)=>{
+    return res.redirect("/login");
+  });
+};
+
+module.exports.singnout = (req, res) => {
+  console.log("cookies", req.cookies);
+  // const { user_id }= req.cookies;
+
+  if(!user_id){
+    return res.redirect('/login');
+  } else{
+   return res.clearCookie("user_id").redirect('/login');
+  }
+};
+
+
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,14 @@ const login_controller=  require('../controllers/login_controller');
 
 router.get('/', homeController.home);
 router.get('/post', post_controller.post);
+
 router.get('/login', login_controller.login);
 router.get('/signup', login_controller.signup);
+
 router.post('/create', login_controller.create);
+router.post('/createSession', login_controller.createSession);
+
+router.get('/profile', login_controller.profile);
+router.get('/sign_out', login_controller.singnout);
 
 module.exports= router;
